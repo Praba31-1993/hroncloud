c2db13b30507d8cb98589b39c8b07b865b0437d7
Praba31-1993 <prabaidymoto@gmail.com>
2025-03-17 19:46:20 +0530

    clikentid

--- app/api/axiosInstance.tsx
+++ app/api/axiosInstance.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 // const base_url = "http://192.168.10.126:8080/employez/";
-const base_url="http://localhost:8080/employez/"
+const base_url="http://localhost:8080/report-services/"
 export const axiosInstance = axios.create({
   baseURL: base_url,
   headers: {
     "Content-Type": "application/json",
-    clientId: "dev",
+    clientId: "demo",
   },
 });
 
