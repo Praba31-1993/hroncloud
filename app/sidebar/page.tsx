efd7c0659038d62b71df7b1b942149924bfa8df8
mohamedsafiullah9553 <mohamedsafiullah9553@gmail.com>
2025-03-19 13:36:40 +0530

    important contact

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -219,7 +219,10 @@ const Sidebar = ({ children }: any) => {
 
         setMenuLists(newList);
         sethideToggle(false);
+       const sidemenu = "1"
+       if(sidemenu == "1"){
         document.cookie = "auth=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+       }
         toast.success(Messages.success.restored);
     };
     const useColors = Colors();
